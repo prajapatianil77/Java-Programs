dc335350e0739c8616d47f2e13cc83a939933ae6
Anil <anilprajapatbkbiet2022@gmail.com>
2024-03-14 14:12:09 +0530

    uploading sum program.

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum {
+    public static void main(String []arr){
+        int a,b;
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter a number = ");
+        a = ob.nextInt();
+        System.out.print("Enter another number = ");
+        b = ob.nextInt();
+        System.out.println("Sum of number is = "+(a+b));
+
+
+    }
+}
